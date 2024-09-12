69f5bd883be645831244e17b01e29dfba7146305
Jack <904916790@qq.com>
2024-09-12 22:12:52 +0800

    day14 - date

---
+++ day14/src/Date.java
@@ -0,0 +1,10 @@
+public class Date {
+    public static void main(String[] args) {
+        java.util.Date d = new java.util.Date();
+        System.out.println(d);//Thu Sep 12 22:07:37 CST 2024
+        System.out.println(d.getTime());// 1726150057319
+
+
+        
+    }
+}
