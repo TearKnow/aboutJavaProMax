7d5b94b6ac3c19601ac7b25bd2e9fe5a823e7ec6
dunpai <dunpai@i9i8.com>
2024-09-18 16:46:40 +0800

    fix

--- day14/src/Date.java
+++ day14/src/Date.java
@@ -1,11 +1,11 @@
-import java.time.Duration;
-import java.time.LocalDateTime;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
 
 public class Date {
     public static void main(String[] args) {
-        java.util.Date d = new java.util.Date();
-        System.out.println(d);//Thu Sep 12 22:07:37 CST 2024
-        System.out.println(d.getTime());// 1726150057319
+        java.util.Date dd = new java.util.Date();
+        System.out.println(dd);//Thu Sep 12 22:07:37 CST 2024
+        System.out.println(dd.getTime());// 1726150057319
 
 
 
